b505b341984f090f9db4fe0e6aaa183faedd6fea
bbrassart <brassart.baptiste@gmail.com>
2017-03-09 09:55:21 +0100

    Style flash message in show To do: style flash messages in index page

--- public/js/application.js
+++ public/js/application.js
@@ -3,5 +3,11 @@
         !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
         mobileNavMenu.setInitialListeners();
         anchorAnimation.setInitialListeners();
+
+        // Hide flash notification if any
+        if ($('#flashMessage').length > 0) {
+            $('#flashMessage').delay(5000).fadeOut('slow');
+        }
+
     })
 })();
