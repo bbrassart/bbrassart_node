8ce00532037a55d37e4eaebd8b0428ce2df9e565
bbrassart <brassart.baptiste@gmail.com>
2017-03-10 10:03:34 +0100

    Upgrade Mongoose version

--- server.js
+++ server.js
@@ -40,6 +40,8 @@ app.set('views', __dirname + '/app/views');
 var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
     replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
 
+
+// Connect with DB
 var envDatabase  = (function(env) {
     switch (env) {
         case 'production':
@@ -49,8 +51,7 @@ var envDatabase  = (function(env) {
     }
 })(process.env.NODE_ENV);
 
-// Connect with DB
-mongoose.connect(envDatabase, options);
+mongoose.connect(process.env.MONGODB_URI, options);
 
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
