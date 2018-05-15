ca17d1d8835798dfc0fe1a44571e84f99ae40721
bbrassart <brassart.baptiste@gmail.com>
2018-05-15 09:26:08 +0200

    Streamline other JS code

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,42 @@
 (function() {
     $(document).ready(function () {
-        !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-        mobileNavMenu.setInitialListeners();
-        anchorAnimation.setInitialListeners();
 
-        // Hide flash notification if any
-        if ($('#flashMessage').length > 0) {
-            $('#flashMessage').delay(5000).fadeOut('slow');
+      var mobileNavMenu = {
+        setInitialListeners: function() {
+          $("select").change(function() {
+            var uri = $(this).find("option:selected").val();
+            if (navigator.userAgent.match(/Android/i))
+              document.location = uri;
+            else
+              window.location = uri;
+          });
         }
+      };
 
+      var anchorAnimation = {
+        setInitialListeners: function() {
+          $('a[href*="#"]:not([href="#"])').click(function() {
+
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+              && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                window.location.hash = this.hash;
+                return false;
+              }
+            }
+          });
+        }
+      };
+      mobileNavMenu.setInitialListeners();
+      anchorAnimation.setInitialListeners();
+      // Hide flash notification if any
+      if ($('#flashMessage').length > 0) {
+         $('#flashMessage').delay(5000).fadeOut('slow');
+      }
     })
 })();
